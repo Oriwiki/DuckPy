279fcd6d31be108f407a65752e0bc4939c649cb5
root <root@DESKTOP-FBGO7DH.localdomain>
2017-10-14 02:04:47 +0900

    Modify model

--- mywiki/models.py
+++ mywiki/models.py
@@ -3,17 +3,18 @@ from django.db import models
 # Create your models here.
 class Page(models.Model):
     title = models.CharField(unique=True, blank=False, max_length=255)
+    namespace = models.PositiveIntegerField()
     backlink = models.TextField(null=True)
     category = models.TextField(null=True)
     is_deleted = models.BooleanField(default=False)
     
 class Revision(models.Model):
     text = models.TextField()
-    createDate = models.DateTimeField(auto_now_add=True, null=True)
+    createDate = models.DateTimeField(auto_now_add=True)
     comment = models.TextField(null=True)
     user = models.ForeignKey('auth.user', on_delete=models.SET_NULL, null=True)
     ip = models.ForeignKey('ip', on_delete=models.SET_NULL, null=True)
-    page = models.ForeignKey('page', on_delete=models.CASCADE, null=True)
+    page = models.ForeignKey('page', on_delete=models.CASCADE)
     
 class Ip(models.Model):
     ip = models.CharField(unique=True, blank=False, max_length=15)
