ca8a75af069ddb1931d746c6caa8e876b746fe2f
root <root@DESKTOP-FBGO7DH.localdomain>
2017-10-14 03:25:25 +0900

    Support view old revision #20

--- mywiki/views.py
+++ mywiki/views.py
@@ -47,7 +47,7 @@ def edit(request, title=None, section=0):
     
         return redirect('/w/' + title)
         
-def view(request, title=None):
+def view(request, title=None, rev=0):
     if request.method == 'GET':
         if title == None:
             if request.path.startswith('/w/'):
@@ -64,9 +64,18 @@ def view(request, title=None):
                 page = Page.objects.get(title=title)
                 Revision(text='Hello, World!', page=page, comment='This is testing revision.', rev=1).save()
                 page_id = page.id
-            
-        input = Revision.objects.filter(page=page_id).order_by('-id').first().text
-    
+                
+        if 'rev' in request.GET:
+            rev = request.GET['rev']        
+                
+        if rev == 0:
+            input = Revision.objects.filter(page=page_id).order_by('-id').first().text
+        else:
+            try:
+                input = Revision.objects.get(page=page_id, rev=rev).text
+            except ObjectDoesNotExist:
+                return HttpResponseNotFound()
+                
         soup = BeautifulSoup(NamuMarkParser(input, title).parse(), 'html.parser')
         return render(request, 'wiki.html', {'parse': soup.prettify(), 'title': title})
         
@@ -75,13 +84,13 @@ def raw(request, title=None, rev=0):
         if title == None:
             return redirect('/')
             
-        if 'rev' in request.GET:
-            rev = request.GET['rev']
-            
         try:
             page_id = Page.objects.get(title=title).id
         except ObjectDoesNotExist:
             return HttpResponseNotFound()
+            
+        if 'rev' in request.GET:
+            rev = request.GET['rev']
         
         if rev == 0:
             return HttpResponse(Revision.objects.filter(page=page_id).order_by('-id').first().text, content_type='text/plain; charset="utf-8"')
